d9c4a03430aa61a380208ba82861adeb4ae150f4
Neto Daniel ZINGA <50544159+dannyclever@users.noreply.github.com>
2023-01-18 19:27:22 +0100

    created the function tocarSom() & ativarDiv to listen the event click and play the sound

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 'use strict';
+
+const sons = {
+    'A': 'boom.wav',
+    'S': 'clap.wav',
+    'D': 'hihat.wav',
+    'F': 'kick.wav',
+    'G': 'openhat.wav',
+    'H': 'ride.wav',
+    'J': 'snare.wav',
+    'K': 'tink.wav',
+    'L': 'tom.wav'
+}
+
 const criarDiv = (texto) => {
     const div = document.createElement('div');
     div.classList.add('key');
@@ -6,3 +19,18 @@ const criarDiv = (texto) => {
     div.id = texto;
     document.getElementById('container').appendChild(div);
 }
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+const tocarSom = (letra) => {
+    const audio = new Audio(`./sounds/${sons[letra]}`);
+    audio.play();
+}
+
+const ativarDiv = (evento) => {
+    const letra = evento.target.id;
+    tocarSom(letra);
+}
+
+exibir(sons);
+document.getElementById('container').addEventListener('click', ativarDiv);
